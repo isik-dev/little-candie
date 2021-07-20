f8355bc894c4b76f11fadde1ba25fd61727fb5b1
islomnumanov <islom4ik.numanov@gmail.com>
2021-07-20 16:32:13 +0900

    modules deleted

--- src/scream.js
+++
@@ -1,6 +0,0 @@
-// 1. Setup scream as the default export
-// 2. Update the import statement in the index.js to use it
-
-const scream = (word) => `${word.toUpperCase()}!`;
-
-export { scream as default };
